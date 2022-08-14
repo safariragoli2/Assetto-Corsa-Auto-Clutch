01f3f486f39ad6bf2be9d06a54832787e87421bd
aerobatic prop manipulator <89122797+safariragoli2@users.noreply.github.com>
2022-08-14 00:47:46 -0500

    Create AutoClutch.py

---
+++ AutoClutch/AutoClutch.py
@@ -0,0 +1,22 @@
+import sys
+import ac
+import acsys
+import pyvjoy
+
+VirtualClutch = pyvjoy.VJoyDevice(1)
+
+def acMain(ac_version):
+    appWindow = ac.newApp("Auto Clutch")
+    ac.setSize(appWindow, 250, 100)
+    text = ac.addLabel(appWindow, "Simply having the app itself enabled\nwill enable the auto clutch, no need\nto have this UI enabled.")
+    ac.setPosition(text, 3,30)
+    
+    return "Auto Clutch"
+
+def acUpdate(deltaT):
+    currentGear = ac.getCarState(0, acsys.CS.Gear)
+    
+    if currentGear == 1: # neutral
+        VirtualClutch.set_axis(pyvjoy.HID_USAGE_X, 32768) # press clutch
+    else:
+        VirtualClutch.set_axis(pyvjoy.HID_USAGE_X, 0) # depress clutch
